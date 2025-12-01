f9c22cee6ff753b3c69bb4c93f1d7a1e7b06ba1d
adeliasb <qa.asbs.lab@gmail.com>
2025-12-01 13:08:01 -0300

    chore(ci): Adição de arquivos de configuração cypress.config.js e package.json

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome", // gera arquivos JSON por teste
+  reporterOptions: {
+    reportDir: "cypress/results", // onde salvar
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "https://dog.ceo/api", // URL base da Dog API
+    specPattern: "cypress/e2e/**/*.cy.js", // localização dos testes
+    supportFile: "cypress/support/e2e.js",
   },
 });
